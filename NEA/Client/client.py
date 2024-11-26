b01839cfd1430342da7bdaa307b352ee629fd85a
aryansharmastudies <aryansharmastudies@gmail.com>
2024-11-26 10:45:07 +0000

    trying to send a file through by converting it to binary

--- NEA/Client/client.py
+++ NEA/Client/client.py
@@ -1,2 +1,7 @@
 # main program.
 # client program which connects to file-server and checks for missing files.
+def convert_to_binary(file_name = "text"):
+    r = open(f"{file_name}.txt", "rb")
+    data = r.read()
+    print(data)
+convert_to_binary()
