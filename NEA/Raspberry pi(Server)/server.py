843378b9ef8629239033a7d5665b2ce5ebc58b63
aryansharmastudies <aryansharmastudies@gmail.com>
2024-12-01 12:43:31 +0000

    yay finally got file transfer working now you can send through text files!

--- NEA/Raspberry pi(Server)/server.py
+++ NEA/Raspberry pi(Server)/server.py
@@ -1,1 +1,18 @@
 # hosts file-server on raspberry pi.
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+print(host)
+s.bind((host, 1234))
+s.listen(2)
+
+while True:
+    clientsocket, addr = s.accept()
+    print(f"connected with {addr}")
+    message = clientsocket.recv(1024)
+    name = input("enter the file name u wanna store it as: ")
+    file = open(name, "wb")
+    file.write(message)
+    file.close()
+    clientsocket.close()
