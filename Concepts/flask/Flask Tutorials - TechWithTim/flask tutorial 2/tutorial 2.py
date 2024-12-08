39884b28bb9653172506b2dcf10f21f119381049
aryansharma <aryansharmastudies@gmail.com>
2024-12-08 12:08:26 +0000

    done with tutorial 3. it was all about getting bootstrap set up... to make a nav bar... and getting inheritance set up

--- Concepts/flask/Flask Tutorials - TechWithTim/flask tutorial 2/tutorial 2.py
+++ Concepts/flask/Flask Tutorials - TechWithTim/flask tutorial 2/tutorial 2.py
@@ -2,9 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>") # in the url if we type localhost:5000/home we are returned with home page.
-def home(name): #represents the homecase...
-    return render_template("index.html", content=name, names=["hazique", "ailie", "riadoor"])
+@app.route("/<name>/<subject>") # in the url if we type localhost:5000/home we are returned with home page.
+def home(name, subject): #represents the homecase...
+    return render_template("index.html", subject=subject, content=name, names=["hazique", "ailie", "riadoor"])
 
 if __name__ == "__main__":
     app.run() 
