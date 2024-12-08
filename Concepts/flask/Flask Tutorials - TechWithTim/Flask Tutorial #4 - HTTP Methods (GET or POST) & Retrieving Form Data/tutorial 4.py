417b4298f58a8fa22335a100feed520fa862d370
aryansharma <aryansharmastudies@gmail.com>
2024-12-08 13:56:07 +0000

    done with tutorial 4. erm all there was is the use of POST to send data from the website to the backend

---
+++ Concepts/flask/Flask Tutorials - TechWithTim/Flask Tutorial #4 - HTTP Methods (GET or POST) & Retrieving Form Data/tutorial 4.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/") # in the url if we type localhost:5000/home we are returned with home page.
+def home(): #represents the homecase...
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"]) # in the url if we type localhost:5000/login we are returned with login page.
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        if user == "":
+            user = "Clown"
+        return redirect(url_for("user", usr=user)) # nm is the dictionary key for name of user input.
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
