8548d0957f0b17b30226ec0c65e650bbdd8843e0
aryansharmastudies <aryansharmastudies@gmail.com>
2025-04-15 18:02:26 +0100

    new ride baby

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kyoto/Desktop/NEA_v2/.venv
+set -gx VIRTUAL_ENV /home/osaka/Documents/NEA_v2/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
